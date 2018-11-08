e6c4e6c4bad003b92f286550943a404c522e0b55
z_llong@163.com <zllong145511>
2018-11-08 23:23:49 +0800

    aliyun

--- Analysis_WeiboUid/WeiboUid.py
+++ Analysis_WeiboUid/WeiboUid.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import datetime
+import time
 import oss2
 from itertools import islice
 
@@ -13,27 +14,40 @@ from func.func_write2file import *
 
 
 def handler(event, context):
-    endpoint = 'oss-cn-beijing-internal.aliyuncs.com'
-    bucket = 'analysis-demo.oss-cn-beijing-internal.aliyuncs.com'
-    creds = context.credentials
-    uid_list_file = 'tmp/weibo_uid_list'
-    uid_list = []
-
     logger = logging.getLogger()
     logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' Start WeiboUid')
     print('--------------------------------------------------------------------------------------')
+    print(get_uid('https://weibo.com/chcedo')['id'] + '\n')
 
+    tmpdir = '/tmp/download/'
+    os.system("rm -rf /tmp/*")
+    os.mkdir(tmpdir)
+    logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' tmpdir is ready')
+
+    endpoint = 'oss-cn-shanghai-internal.aliyuncs.com'
+    bucketname = 'leo-demo'
+    creds = context.credentials
+    url_list_file = 'weibo_url_list'
     auth = oss2.StsAuth(creds.accessKeyId, creds.accessKeySecret, creds.securityToken)
-    bucket = oss2.Bucket(auth, endpoint, bucket)
+    bucket = oss2.Bucket(auth, endpoint, bucketname)
+    bucket.get_object_to_file('configuration/weibo_url_list', tmpdir + url_list_file)
+    logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' weibo_url_list is ready')
 
-    bucket.get_object_to_file('configuration/weibo_url_list', uid_list_file)
+    uid_list_file = tmpdir+'weibo_uid_list'
+    uid_list = []
 
-    with open(uid_list_file, 'r') as f_uid:
+    with open(tmpdir + url_list_file, 'r') as f_uid:
         l_uid = f_uid.readlines()
     for i in range(0, len(l_uid)):
+        print('---' + str(i+1) + '/' + str(len(l_uid)) + '---')
+        print('---' + l_uid[i].rstrip('\n'))
+        print('---' + get_uid(l_uid[i].rstrip('\n'))['id'])
+        time.sleep(1)
         uid_list.append(get_uid(l_uid[i].rstrip('\n'))['id'] + '\n')
 
     write_to_file(uid_list, uid_list_file, 'w')
+
+    bucket.put_object(bucketname+'/configuration/weibo_uid_list', uid_list_file)
 
     user_list = ['leo.zhai@bizfocus.cn']
     sub = "现在时间： " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
--- Analysis_WeiboUid/func/func_requestuid.py
+++ Analysis_WeiboUid/func/func_requestuid.py
@@ -1,6 +1,12 @@
 #  -*-  coding:  UTF-8  -*-
 import requests
-import json
+from retrying import retry
+
+
+#wait_fixed 设置失败重试的间隔时间
+#wait_random_min, wait_random_max 设置失败重试随机性间隔时间
+#stop_max_delay 设置失败重试的最大时间, 单位毫秒，超出时间，则停止重试
+@retry(wait_fixed=1000, stop_max_delay=10000)
 
 def get_uid(p_url):
     url="http://api01.idataapi.cn:8000/profile/weibo"
