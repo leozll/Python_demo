095d405e187d074e3f269416508ffe4815285b68
z_llong@163.com <zllong145511>
2018-11-19 00:49:36 +0800

    aliyun

---
+++ weibo/request_json/parse_json.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import logging
+import datetime
+import time
+import sys
+import os
+import json
+import oss2
+from tablestore import *
+
+sys.path.append(os.curdir + '/func/packages/')
+from func.func_requestweibocontent import *
+from func.func_sendmail import *
+from func.func_nosql import *
+
+
+def handler(event, context):
+    evt = json.loads(event)
+    print(evt)
+    datekey_uid = int((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y%m%d'))
+    oss_json_path = "result/weibo_json/"
+    oss_csv_path = "result/weibo_csv/"
+
+    logger = logging.getLogger()
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + '--------------------------------------------------------------------------------------')
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Start parse the oss json file......')
+
+    bucketname = evt['events'][0]['oss']['bucket']['name']
+    region = evt['events'][0]['region']
+    endpoint = 'oss-{0}.aliyuncs.com'.format(region)
+    creds = context.credentials
+    access_key_id = creds.accessKeyId
+    access_key_secret = creds.accessKeySecret
+    auth = oss2.StsAuth(access_key_id, access_key_secret, creds.securityToken)
+    bucket = oss2.Bucket(auth, endpoint, bucketname)
+
+    # connect to the table store
+    instance_name = 'leo-ts-hangzhou'
+    ts_log = 'table_store.log'
+    tbl = 'log_parse_json'
+    err_tbl = 'log_parse_json_error'
+    #ts_endpoint = 'https://{0}.{1}.ots-internal.aliyuncs.com'.format(instance_name, region)
+    #ots_client = OTSClient(ts_endpoint, access_key_id, access_key_secret, instance_name, socket_timeout=10,logger_name=ts_log)
+
+    access_key_id = 'LTAIi3mUQglhfB3D'
+    access_key_secret = 'y4acQJO0QkoCbaPipQDFD2idfNOVqJ'
+    ts_endpoint = 'https://{0}.{1}.ots.aliyuncs.com'.format(instance_name, region)
+    ots_client = OTSClient(ts_endpoint, access_key_id, access_key_secret, instance_name, socket_timeout=10,logger_name=ts_log)
+
+
+
+    # get object from event when post object
+    json_key = evt['events'][0]['oss']['object']['key']
+    json_file_meta = bucket.get_object_meta(json_key)
+    content_length = json_file_meta.content_length
+    last_modified = json_file_meta.last_modified
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 1: found file ' + json_key + '(modified: ' + str(last_modified) + ')')
+    pk = [('datekey', datekey_uid), ('jsonkey', json_key)]
+    attr = [('name', json_key),
+            ('length', content_length),
+            ('last_modified', last_modified),
+            ('status', 'ready'),
+            ('update_timestamp',(datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+            ('update_time', int(time.time()))]
+    put_row(ots_client, tbl, pk, attr)
+
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 2: start to parse ' + json_key )
+
+    #read the file
+    print (bucket.get_object(json_key).read().decode('utf-8'))
+    content = json.loads(bucket.get_object(json_key).read().decode('utf-8').replace("'", "\""))
+    content_list = []
+    content_list.append(content['id'])  # 文章id
+    content_list.append(content['url'])  # 文章链接
+    content_list.append(content['mblog']['localPublishDateStr'])  # 发布时间／北京时间
+    if content['edit_at'] is not None:
+        content_list.append(content['edit_at'])  # 最后编辑时间
+    else:
+        content_list.append('')
+    content_list.append(content['shareCount'])  # 转发数
+    content_list.append(content['commentCount'])  # 评论数
+    content_list.append(content['likeCount'])  # 点赞数
+    content_list.append(content['content'].replace("\n", ""))  # 文章内容
+    content_list.append(content['mblog']['source'])  # 来自
+    if content['imageURLs'] is not None:
+        content_list.append(';'.join(content['imageURLs']))  # 图片链接
+    else:
+        content_list.append('')
+    if 'retweeted_status' in content['mblog']:
+        content_list.append(content['mblog']['retweeted_status']['user']['id'])  # 转发作者uid
+        content_list.append(content['mblog']['retweeted_status']['id'])  # 转发文章id
+        content_list.append(content['mblog']['retweeted_status']['created_at'])  # 转发文章发布时间
+        content_list.append(content['mblog']['retweeted_status']['text'].replace("\n", ""))  # 转发内容
+        content_list.append(content['mblog']['retweeted_status']['reposts_count'])  # 转发文章转发数
+        content_list.append(content['mblog']['retweeted_status']['comments_count'])  # 转发文章评论数
+        content_list.append(content['mblog']['retweeted_status']['attitudes_count'])  # 转发文章点赞数
+        if content['mblog']['retweeted_status']['pic_urls'] is not None:
+            if content['mblog']['retweeted_status']['pic_urls'] != []:
+                content_list.append(';'.join(
+                    list(content['mblog']['retweeted_status']['pic_urls'][0].values())))  # 转发内容图片
+        else:
+            content_list.append('')
+        content_list.append(content['mblog']['retweeted_status']['source'])  # 转发文章来自
+    else:
+        content_list.append('')
+        content_list.append('')
+        content_list.append('')
+        content_list.append('')
+        content_list.append('')
+        content_list.append('')
+    content_list.append((datetime.datetime.utcnow() + datetime.timedelta(
+        hours=8)))
+
+    csv_file = json_key.replace('json','csv')
+
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 3: unload result to ' + csv_file )
+    bucket.put_object(csv_file, content_list)
+    pk = [('datekey', datekey_uid), ('jsonkey', json_key)]
+    attr = [('name', json_key),
+            ('length', content_length),
+            ('last_modified', last_modified),
+            ('status', 'finish'),
+            ('update_timestamp',(datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+            ('update_time', int(time.time()))]
+    put_row(ots_client, tbl, pk, attr)
+
+
+if __name__ == "__main__":
+    handler('1', '2')
---
+++ weibo/request_json/request_json.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+import logging
+import datetime
+import time
+import sys
+import os
+import oss2
+
+sys.path.append(os.curdir + '/func/packages/')
+from func.func_requestweibocontent import *
+from func.func_sendmail import *
+from func.func_nosql import *
+
+
+def handler(event, context):
+    uid_count = 20
+    d_list = [201811]
+
+
+    runtime_duration = 550
+    tmpdir = '/tmp/weibo/'
+    tmp_content_list = tmpdir + 'weibo_content_list.csv'
+
+    # oss
+    region = "cn-hangzhou-internal"
+    bucketname = "leo-hangzhou"
+    oss_uid_list = "configuration/weibo_uid_list"
+    oss_json_path = "result/weibo_json/"
+
+    #table storehang'zhou
+    instance_name = 'leo-ts-hangzhou'
+    ts_log = 'table_store.log'
+    tbl = 'log_weibo'
+    err_tbl = 'log_weibo_error'
+    ts_region = 'cn-hangzhou.ots'
+    ts_endpoint = 'https://{0}.{1}.aliyuncs.com'.format(instance_name, ts_region)
+    access_key_id = 'LTAIi3mUQglhfB3D'
+    access_key_secret = 'y4acQJO0QkoCbaPipQDFD2idfNOVqJ'
+
+    runtime_start = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+    datekey_uid = int((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y%m%d'))
+    os.system("rm -rf /tmp/*")
+    os.mkdir(tmpdir)
+
+    logger = logging.getLogger()
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + '--------------------------------------------------------------------------------------')
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Start WeiboUid')
+
+
+    endpoint = 'oss-{0}.aliyuncs.com'.format(region)
+    creds = context.credentials
+    auth = oss2.StsAuth(creds.accessKeyId, creds.accessKeySecret, creds.securityToken)
+    bucket = oss2.Bucket(auth, endpoint, bucketname)
+
+
+    # get the weibo uids
+    uids = []
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 1 : get weibo uids from oss')
+    # if the uid_list is not exists
+    if bucket.object_exists(oss_uid_list):
+        content = bucket.get_object(oss_uid_list)
+        uids = content.read().decode('utf-8').split("\r\n")
+        uids.sort()
+    else:
+        logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' ERROR!!! cant find uids file from oss')
+        exit(1)
+    uids=["1817559703"]
+
+    # initialize the uids
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 2 : initialize the uids status')
+    # connect to the table store
+    ots_client = OTSClient(ts_endpoint, access_key_id, access_key_secret, instance_name, socket_timeout=10,logger_name=ts_log)
+    for uid in uids:
+        pk = [('uid', int(uid))]
+        #if no this uid
+        if get_col(ots_client, tbl, pk, ['status'], None) == 'NoData':
+            attr = [('status', 'waiting'),
+                    ('update_timestamp',(datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+                    ('update_time', int(time.time()))]
+            put_row(ots_client, tbl, pk, attr)
+
+    # get last runtime
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 3 : get the last runtime status')
+    pk = [('uid', datekey_uid)]
+    checkpoint = 0
+    last_runtime = int(time.time())
+    last_runtimestamp = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+    # if no last runtime status record, initialize
+    if get_col(ots_client, tbl, pk, ['update_time'], None) == 'NoData':
+        attr = [('name', datekey_uid), ('status', ','.join(uids)),
+                ('update_timestamp', last_runtimestamp),
+                ('update_time', last_runtime),
+                ('checkpoint', checkpoint)]
+        put_row(ots_client, tbl, pk, attr)
+    else:
+        # if the uids list is changed, initialize the log table
+        if get_col(ots_client, tbl, pk, ['status'], None) != ','.join(uids):
+            # initialize
+            attr = [('name', datekey_uid), ('status', ','.join(uids)),
+                    ('update_timestamp', last_runtimestamp),
+                    ('update_time', last_runtime),
+                    ('checkpoint', checkpoint)]
+            put_row(ots_client, tbl, pk, attr)
+        #if no change, get the last checkpoint
+        else:
+            cols = ['checkpoint']
+            checkpoint = get_col(ots_client, tbl, pk, cols, None)
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' last runtime checkpoint is ' + str(checkpoint))
+
+    # loop uid to analysis
+    logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 4 : start to analysis weibo content')
+    #if all the uids are analysed today
+    if checkpoint >= len(uids) and get_col(ots_client, tbl, pk, ['name'], None) == datekey_uid:
+        logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 5 :  all the uids are analysed today! exit the script! (checkpoint:' + str(checkpoint) + ' totally:' + str(len(uids)) + ')')
+        return 'Congratulations! no uids are left! ' + (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime(
+            '%Y-%m-%d %H:%M:%S')
+    #if this is the last run
+    if checkpoint + uid_count > len(uids):
+        endp = len(uids)
+    else:
+        endp = checkpoint + uid_count
+
+    #loop current run
+    for i in range(checkpoint, endp):
+        logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 5 (' + str(i + 1) + '/' + str(i + 1 + uid_count) + ') : start to analyse uid: ' + uids[i])
+        request_status = ''
+        #loop the months
+        for d in d_list:
+            try:
+                loop_starttime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+                # get the weibo content
+                request_page = 1
+                global  content_j
+                page_hasNext = True
+                # 怎么判断pageToken
+                while page_hasNext:
+                    content_j = weibo_content(uids[i], d ,request_page)
+                    page_hasNext = content_j['hasNext']
+                    request_status = content_j["retcode"]
+
+                    print ('start to unload page ' + str(request_page) + '......')
+                    for content_data in content_j['data']:
+                        publish_year = content_data['mblog']['localPublishDateStr'][0:4]
+                        publish_month = content_data['mblog']['localPublishDateStr'][5:7]
+                        publish_date = content_data['mblog']['localPublishDateStr'][8:10]
+                        json_file = oss_json_path + publish_year + '/' + publish_month + '/' + publish_date + '/' + str(uids[i]) + '/'+ content_data['id'] + '.json'
+                        #bucket.put_object(json_file, json.dumps(content_data))
+                        bucket.put_object(json_file, '{0}'.format(content_data))
+
+                    print('page ' + str(request_page) + " finished......")
+                    time.sleep(1)
+                    request_page = request_page + 1
+
+                print(str(uids[i]) + str(d) + '解析完毕......')
+            except Exception  as e:
+                request_status = str(e)
+                pk = [('uid', int(uids[i])), ('datekey', datekey_uid)]
+                attr = [('error_log', str(e)),
+                        ('update_timestamp',(datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+                        ('update_time', int(time.time()))]
+                put_row(ots_client, err_tbl, pk, attr)
+                logger.info((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + '"' + str(uids[i]) + '"解析失败????...：' + str(e))
+
+        # if finished
+        pk = [('uid', int(uids[i]))]
+        attr = [('name', int(uids[i])),
+                ('status', str(request_status)),
+                ('update_timestamp',(datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+                ('update_time', int(time.time()))]
+        put_row(ots_client, tbl, pk, attr)
+        # if finished
+        pk = [('uid', datekey_uid)]
+        attr = [('name', datekey_uid),
+                ('status', ','.join(uids)),
+                ('update_timestamp', (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')),
+                ('update_time', int(time.time())),
+                ('checkpoint', checkpoint + i + 1)]
+        put_row(ots_client, tbl, pk, attr)
+
+    return 'Congratulations! ' + (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+if __name__ == "__main__":
+    handler('1', '2')
