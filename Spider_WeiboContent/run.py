06349bcb5c9c38aa3dd64f9a08385bdff2131ceb
z_llong@163.com <zllong145511>
2018-12-10 23:49:58 +0800

    aliyun

--- Spider_WeiboContent/run.py
+++ Spider_WeiboContent/run.py
@@ -1,6 +1,5 @@
 #-*-coding:utf8-*-
 
-import re
 import string
 import sys
 import os
@@ -10,120 +9,69 @@ import requests
 import shutil
 import time
 from lxml import etree
-
+import csv
+import random
+import datetime
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 #if(len(sys.argv)>=2):
 #        user_id = (int)(sys.argv[1])
 #else:
 #        user_id = (int)(raw_input(u"please_input_id: "))
-user_id = 1826792401
-cookie = {"Cookie": "ALF=1544581055; _T_WM=9ede271cb36cf03b6b037a1dccfe8eb4; WEIBOCN_WM=5091_90006; WEIBOCN_FROM=1110006030; SCF=Ag-aVgEpqjL43lYMyv7OPwcmwwuxAsqt9DlUN5XCz5AfCuWbm1evjABjivDn9iiZZSzCAu37AJO9T0S7VXj61Zc.; SUB=_2A2527JzCDeRhGedG7lMR9inNyTuIHXVSLiSKrDV6PUJbktAKLW3CkW1NUT3ACD_WH36PC9t6KvHc0S38R4j3JZhc; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhqzLp519MwEAZep7gvRWzs5JpX5K-hUgL.Fo2RSK27SoMpeoM2dJLoIERLxKMLBozL1KMLxK.L1-BLBKzLxKML1-2L1hBLxK.LBo2LB.eLxKBLBonLBoyk; SUHB=0V5UouQ77OZ9JH; SSOLoginState=1541991570; td_cookie=18446744072992254319; MLOGIN=1; M_WEIBOCN_PARAMS=uicode%3D20000174"}
-url = 'http://weibo.cn/u/%d?filter=1&page=1'%user_id
-html = requests.get(url, cookies = cookie).content
-print (u'user_id和cookie读入成功')
-selector = etree.HTML(html)
-pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
-pageNum=10
+headers = {"Cookie": "SINAGLOBAL=9336210156603.25.1536404794441; UOR=,,login.sina.com.cn; _s_tentry=login.sina.com.cn; Apache=5828895455932.55.1544019181873; ULV=1544019181932:51:3:2:5828895455932.55.1544019181873:1544011518789; login_sid_t=6b76ad6d88fdb757291ed33da16c236a; cross_origin_proto=SSL; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWp-N25.LI6Pvm6x9N549X05JpX5K2hUgL.FoqReK-c1K-p1K52dJLoI7YLxK.L1h-L1h9RdLvzx2nt; ALF=1575555202; SSOLoginState=1544019203; SCF=Ag9BwHY1vJCyYgSSCCi0oxmq3VPC68qoF4Z3UzC63-SfdQG6vT0NEdmd5eYoClnHFHYYI4hRxC-3gi2nzIoNg0E.; SUB=_2A25xA61UDeRhGeBG6lcX-SvNwjyIHXVSeJmcrDV8PUNbmtBeLWXEkW9NRjezh5PtW4DEQ3xuCmlBxaW1E4pGhuSE; SUHB=0oUosgTj4dagi0; un=17825380369; wvr=6"}
 
-result = ""
-urllist_set = set()
-word_count = 1
-image_count = 1
+for pagenum in range(0,100):
+    tmp_profile_list = 'hufu'
 
-print (u'ready')
-print (pageNum)
-sys.stdout.flush()
+    #meizhuang
+    #url = 'https://s.weibo.com/user?q=%E7%BE%8E%E5%A6%86&Refer=weibo_user&page='+str(pagenum)
 
-times = 5
-one_step = pageNum//times
-for step in range(times):
-    if step < times - 1:
-        i = step * one_step + 1
-        j =(step + 1) * one_step + 1
-    else:
-        i = step * one_step + 1
-        j =pageNum + 1
-    print (i,j)
-    for page in range(i, j):
-        #获取lxml页面
-        try:
-            url = 'http://weibo.cn/u/%d?filter=1&page=%d'%(user_id,page)
-            lxml = requests.get(url, cookies = cookie).content
-            #文字爬取
-            selector = etree.HTML(lxml)
-            content = selector.xpath('//span[@class="ctt"]')
-            for each in content:
-                text = each.xpath('string(.)')
-                if word_count >= 3:
-                    text = "%d: "%(word_count - 2) +text+"\n"
-                else :
-                    text = text+"\n\n"
-                result = result + text
-                word_count += 1
-            print (page,'word ok')
-            sys.stdout.flush()
-            soup = BeautifulSoup(lxml, "lxml")
-            urllist = soup.find_all('a',href=re.compile(r'^http://weibo.cn/mblog/oripic',re.I))
-            urllist1 = soup.find_all('a',href=re.compile(r'^http://weibo.cn/mblog/picAll',re.I))
-            for imgurl in urllist:
-                imgurl['href'] = re.sub(r"amp;", '', imgurl['href'])
-        #       print imgurl['href']
-                urllist_set.add(requests.get(imgurl['href'], cookies = cookie).url)
-                image_count +=1
-            for imgurl_all in urllist1:
-                html_content = requests.get(imgurl_all['href'], cookies = cookie).content
-                soup = BeautifulSoup(html_content, "lxml")
-                urllist2 = soup.find_all('a',href=re.compile(r'^/mblog/oripic',re.I))
-                for imgurl in urllist2:
-                    imgurl['href'] = 'http://weibo.cn' + re.sub(r"amp;", '', imgurl['href'])
-                    urllist_set.add(requests.get(imgurl['href'], cookies = cookie).url)
-                    image_count +=1
-                image_count -= 1
-            print (page,'picurl ok')
-        except:
-            print (page,'error')
-        print (page, 'sleep')
-        sys.stdout.flush()
-        time.sleep(60)
-    print (u'正在进行第', step + 1, u'次停顿，防止访问次数过多')
-    time.sleep(300)
+    #shishang
+    url = 'https://s.weibo.com/user?q=%E6%97%B6%E5%B0%9A&Refer=weibo_user&page=' + str(pagenum)
 
-try:
-    fo = open(os.getcwd()+"/%d"%user_id, "wb")
-    fo.write(result)
-    word_path=os.getcwd()+'/%d'%user_id
-    print (u'文字微博爬取完毕')
-    link = ""
-    fo2 = open(os.getcwd()+"/%s_image"%user_id, "wb")
-    for eachlink in urllist_set:
-        link = link + eachlink +"\n"
-    fo2.write(link)
-    print (u'图片链接爬取完毕')
-except:
-    print (u'存放数据地址有误')
-sys.stdout.flush()
+    # hufu
+    url = 'https://s.weibo.com/user?q=%E6%8A%A4%E8%82%A4&Refer=weibo_user&page=' + str(pagenum)
 
-if not urllist_set:
-    print (u'该用户原创微博中不存在图片')
-else:
-    #下载图片,保存在当前目录的pythonimg文件夹下
-    image_path=os.getcwd()+'/weibo_image'
-    if os.path.exists(image_path) is False:
-        os.mkdir(image_path)
-    x = 1
-    for imgurl in urllist_set:
-        temp= image_path + '/%s.jpg' % x
-        print (u'正在下载第%s张图片' % x)
-        try:
-        # urllib.urlretrieve(urllib2.urlopen(imgurl).geturl(),temp)
-            r = requests.get(imgurl, stream=True)
-            if r.status_code == 200:
-                with open(temp, 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-        except:
-            print (u"该图片下载失败:%s"%imgurl)
-        x += 1
-print (u'原创微博爬取完毕，共%d条，保存路径%s'%(word_count - 3,word_path))
-print (u'微博图片爬取完毕，共%d张，保存路径%s'%(image_count - 1,image_path))
+    r = requests.get(url, headers = headers,verify=False)
+    print (u'cookie读入成功')
+
+    #r.encoding = r.apparent_encoding  # 配置编码
+    html = etree.HTML(r.text)
+    urls = html.xpath('//a[@class="name"]/@href')
+    l = len(urls)
+    infos = html.xpath('//div[@class="info"]')
+
+    for n in range(0,l):
+        info = infos[n]
+
+        url = urls[n]
+        uid = info.xpath('p/span/a/@href')[0].split('/')[3]
+        name = info.xpath('div/a[@class="name"]')[0].xpath('string(.)').strip()
+        if len(info.xpath('div/a[@target="_blank"]/@title'))==0:
+            title = ''
+        else:
+            title = info.xpath('div/a[@target="_blank"]/@title')[0]
+        addr = info.xpath('p')[0].xpath('string(.)').replace('个人主页','').strip()
+        if len(info.xpath('p')[1].xpath('span')) == 0:
+            com = info.xpath('p')[1].xpath('string(.)').strip()
+        else :
+            com = ''
+        follow = info.xpath('p/span/a/text()')[0]
+        fans = info.xpath('p/span/a/text()')[1]
+        p = info.xpath('p/span/a/text()')[2]
+        comment = ''
+        label = ''
+        for c in info.xpath('p/text()'):
+            if c.startswith(u"简介："):
+                comment = c
+        for c in info.xpath('p'):
+            if c.text == u"标签：":
+                #label = c.xpath('string(.)').strip()
+                label = " ".join(c.xpath('a/text()'))
+
+        result = "|".join([uid,url,name,title,addr,com,follow,fans,p,comment,label])
+
+        with open(tmp_profile_list, 'a', newline='',encoding='utf-8') as f:
+            f.write(result+'\n')
+    print (str(pagenum)+' is ok...'+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    time.sleep(random.randint(5,10))
--- spider_website/reqeust_zhixiaoren.py
+++ spider_website/reqeust_zhixiaoren.py
@@ -4,14 +4,16 @@ import datetime
 import time
 import oss2
 import requests
+import logging
 from lxml import etree
 
 
 def handler(event, context):
+    max_pages_num = 100
+
     url_prefix = "http://news.zhixiaoren.com/list/1_"
     homepage_url = url_prefix + "1.html"
-    is_initialization = 1
-    oss_html_path = "process/zhixiaoren_html/"
+    oss_url_path = "process/zhixiaoren_url/"
 
     region = "cn-hangzhou-internal"
     bucketname = "leo-hangzhou"
@@ -19,7 +21,9 @@ def handler(event, context):
     creds = context.credentials
     auth = oss2.StsAuth(creds.accessKeyId, creds.accessKeySecret, creds.securityToken)
     bucket = oss2.Bucket(auth, endpoint, bucketname)
-
+    logger = logging.getLogger()
+    logger.info(
+        (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' Step 1: Start to request news.zhixiaoren.com')
     try:
         r = requests.get(homepage_url, timeout=10)  # 请求超时时间为10秒
         r.raise_for_status()  # 如果状态不是200，则引发异常
@@ -28,24 +32,30 @@ def handler(event, context):
         # get pages number
         pages_nums = html.xpath('//a[@class="pagesNum"]/text()')
         pages_num = int(pages_nums[0].split('/')[1].strip())
-        print(pages_num)
+        pages_num = min(pages_num,max_pages_num)
+        logger.info(
+            (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime(
+                '%Y-%m-%d %H:%M:%S') + ' Step 2: Start to loop ' + str(pages_num) + ' pages')
         for p in range(0, pages_num):
+            print ('starting page ' + str(p+1))
             page_url = url_prefix + str(p + 1) + ".html"
             page_r = requests.get(page_url, timeout=10)  # 请求超时时间为10秒
             page_r.raise_for_status()  # 如果状态不是200，则引发异常
             page_r.encoding = page_r.apparent_encoding  # 配置编码
             page_html = etree.HTML(page_r.text)
-            print(page_html)
             # get news list
             news_list = page_html.xpath('//ul[@class="b-TopNew-ul fix"]/li/div/a/@href')
             for news_url in news_list:
-                news_r = requests.get(news_url, timeout=10)  # 请求超时时间为10秒
-                news_r.raise_for_status()  # 如果状态不是200，则引发异常
-                news_r.encoding = news_r.apparent_encoding  # 配置编码
-                html_file = oss_html_path + news_url
-                bucket.put_object(html_file, '{0}'.format(news_r.text))
+                url_file = oss_url_path + news_url.replace('http://news.zhixiaoren.com/','')
+                bucket.put_object(url_file, '{0}'.format(news_url))
+            logger.info(
+                (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime(
+                    '%Y-%m-%d %H:%M:%S') + ' Step 3: (' + str(p+1) + '/' + str(pages_num+1) + ') Page' + str(p+1) + ' is finished...')
         return p
     except Exception as e:
+        logger.info(
+            (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime(
+                '%Y-%m-%d %H:%M:%S') + '产生异常!!!' + str(e))
         return "产生异常" + str(e)
 
 
